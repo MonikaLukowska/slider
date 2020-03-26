d002e2b4b8446c68ef065327ee8857728ed8ce75
Sebastian <monika.lukowska@gmail.com>
2020-03-26 20:35:49 +0100

    complete js logic

--- index.js
+++ index.js
@@ -1,1 +1,88 @@
-const images = ['img/img1.jpg', 'img/img1.jpg', 'img/img1.jpg'];
+const images = ['img/img1.jpg', 'img/img2.jpg', 'img/img3.jpg'];
+const slide = document.querySelector('.slide-img');
+const dots = [...document.querySelectorAll('.dot')];
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const btnPlay = document.querySelector('.play');
+let active = 0;
+const time = 2000;
+let play = false;
+let index;
+
+//change active dot
+
+const changeDot = () => {
+    const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+    dots[activeDot].classList.remove('active');
+    dots[active].classList.add('active');
+}
+
+//change slide image    
+
+const changeSlide = () => {
+slide.src = images[active];
+changeDot();
+active++;
+
+if(active === images.length){
+    active = 0;
+}
+
+console.log(active)
+}
+
+//change slide on dot click
+
+dots.forEach(dot => dot.addEventListener('click', () => {
+    const currentDot = dots.indexOf(dot);
+    console.log(play)
+    if (play === true) {
+        
+        clearInterval(index);
+    }
+    active = currentDot;
+    console.log(active)
+    slide.src = images[active];
+    changeDot();
+    if(play === true){
+         setInterval(changeSlide,time);
+         
+    }  
+    
+}))
+
+//next prev on click
+
+const nextPrev = (e) => {
+
+
+
+if (e.target.classList.contains('next') || e.target.classList.contains('prev')){
+    
+    if (play === true) {
+        clearInterval(index);
+        active--;
+    }
+    if(e.target.classList.contains('next')) {
+        active === images.lenght -1 ? active = 0 : active++;
+    }else if(e.target.classList.contains('prev')){
+        active === 0 ? active = images.length -1 : active--;
+    }
+    slide.src = images[active];
+    changeDot(); 
+    if(play === true){
+        index = setInterval(changeSlide,time);
+       
+   } 
+} 
+console.log
+}
+
+next.addEventListener('click', nextPrev);
+prev.addEventListener('click', nextPrev)
+btnPlay.addEventListener('click', () => {
+     index = setInterval(changeSlide,time);
+     play = true;
+} )
+
+
